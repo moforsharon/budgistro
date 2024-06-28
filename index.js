a7ad0f5039ad6a1ae0e489a3cb419a3adec2412d
moforsharon <moforsharon55@gmail.com>
2024-06-28 14:22:01 +0100

    made some modifications

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { registerRootComponent } from 'expo';
 import App from './App';
+import './index.css';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
 
 registerRootComponent(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
 // webpack.config.js
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
+
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+        description: 'A budget management app',
+      },
+      link: [
+        { rel: 'manifest', href: '/manifest.json' },
+        { rel: 'icon', href: '/assets/favicon.png', sizes: '192x192' },
+        { rel: 'icon', href: '/assets/favicon.png', sizes: '512x512' }
+      ]
+    })
+  );
+
   return config;
 };
