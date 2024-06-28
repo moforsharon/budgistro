857dedec191b564eb78cd3f5ac2e328d5dea2412
moforsharon <moforsharon55@gmail.com>
2024-06-28 15:08:09 +0100

    made some modifications

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 import './src/index.css';
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  const installButton = document.getElementById('installButton');
+  if (installButton) {
+    installButton.style.display = 'block'; // Show the install button
+  }
+});
+
+const installButton = document.getElementById('installButton');
+if (installButton) {
+  installButton.addEventListener('click', (e) => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+}
+
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
