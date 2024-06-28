be5e602e02c68f3924cc09d7e235402b8f51614a
moforsharon <moforsharon55@gmail.com>
2024-06-28 14:41:34 +0100

    made some modifications

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-// webpack.config.js
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
@@ -18,6 +19,13 @@ module.exports = async function(env, argv) {
         { rel: 'manifest', href: '/manifest.json' },
         { rel: 'icon', href: '/assets/favicon.png', sizes: '192x192' },
         { rel: 'icon', href: '/assets/favicon.png', sizes: '512x512' }
+      ]
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'manifest.json'), to: config.output.path },
+        { from: path.resolve(__dirname, 'service-worker.js'), to: config.output.path },
+        { from: path.resolve(__dirname, 'assets'), to: path.join(config.output.path, 'assets') }
       ]
     })
   );
